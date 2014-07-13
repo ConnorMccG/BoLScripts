8c494df38383da32720fb1b66d53b55e53ee3eb4
ConnorMccG <royalsconnor@oulook.com>
2014-07-13 16:24:45 +0100

    Update AkaliRevenge.lua

--- AkaliRevenge.lua
+++ AkaliRevenge.lua
@@ -16,6 +16,21 @@ require "VPrediction"
 -- Target Selector thingy --
 local ts
 
+local AUTO_UPDATE = true
+local VERSION = 1.00
+
+
+if AUTO_UPDATE then
+    local server_version = tonumber(GetWebResult("raw.github.com", "/ConnorMccG/BoLScripts/master/version/AkaliRevenge.version")) -- I have a file on my github which just shows the newest version, what I'm doing here is downloading it and comparing with the constant value above and if there is a newer version then go through the update process
+    if server_version > VERSION then -- compare
+        PrintChat("Script is outdated. Updating to version: " .. server_version .. "...")
+        DownloadFile("https://raw.github.com//ConnorMccG/BoLScripts/master/AkaliRevenge.lua", SCRIPT_PATH .. "AkaliRevenge", function() -- Download the new script (we're literally overwriting the current one)
+            PrintChat("Script updated. Please reload (F9).")
+        end)
+    end
+    if server_version > VERSION then return end -- Quit out the script as it will cause issues
+end
+
 
 
  
@@ -84,7 +99,7 @@ function AkalisMenu()
 			
 end
 
--- Items --
+-- Full menu --
  function ItemManagement()	
 		BilgeWaterCutlass = GetInventorySlotItem(3144)
 		BilgeWaterCutlassR = (BilgeWaterCutlass ~= nil and myHero:CanUseSpell(BilgeWaterCutlass))
